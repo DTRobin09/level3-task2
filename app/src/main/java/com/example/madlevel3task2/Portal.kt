216dc1fb74941b7c2d61daeca0ffdf51ef25d64a
Robin <dtr.robin09@gmail.com>
2020-10-09 01:53:16 +0200

    Added a basic adapter class and a Portal object class

---
+++ app/src/main/java/com/example/madlevel3task2/Portal.kt
@@ -0,0 +1,7 @@
+package com.example.madlevel3task2
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Portal(val name: String, val url: String): Parcelable
