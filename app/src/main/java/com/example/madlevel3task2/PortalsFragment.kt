ff2d66d1d328fdb5a6ec2906277e1fbb3d2fd58b
Robin <dtr.robin09@gmail.com>
2020-10-09 11:54:30 +0200

    MainActivity fab and PortalsFragment.kt initviews

--- app/src/main/java/com/example/madlevel3task2/PortalsFragment.kt
+++ app/src/main/java/com/example/madlevel3task2/PortalsFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_portals.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class PortalsFragment : Fragment() {
+
+    private val portals = arrayListOf<Portal>()
+    private val portalAdapter = PortalAdapter(portals)
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +30,12 @@ class PortalsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initViews()
     }
+
+    private fun initViews() {
+        rvPortals.layoutManager = GridLayoutManager(context, 2)
+        rvPortals.adapter = portalAdapter
+    }
+
 }
